4e4ab8e0d7dc2962151f7175f5a1474dc15c5961
alishaqueen <89334519+alishaqueen@users.noreply.github.com>
2022-01-06 18:35:41 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -61,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎧 ᴛʜɪs sᴏɴɢ ɪs ᴜᴘʟᴏᴀᴅᴇᴅ ᴠɪᴀ\n💥 [Aʙʜɪᴍᴀɴʏᴜ Sɪɴɢʜ Rᴀɴᴀᴡᴀᴛ](t.me/Venom_Hai_Hum) ...**"
+        rep = "**🎧 ᴛʜɪs sᴏɴɢ ɪs ᴜᴘʟᴏᴀᴅᴇᴅ ᴠɪᴀ\n💥 [Aʙʜɪᴍᴀɴʏᴜ Sɪɴɢʜ Rᴀɴᴀᴡᴀᴛ](t.me/Itz_Venom_xD) ...**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
